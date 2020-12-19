d864de90188114b6f86c4ab22ab63e0ca536ca40
Jihee <warmgrey32@gmail.com>
2020-12-19 14:55:56 -0500

    update schema

--- index.js
+++ index.js
@@ -1,36 +1,62 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
 const port = 5000
-const bodyparser = require('body-parser');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require("./models/User");
 
-
-
-app.use(bodyparser.urlencoded({extended: true}));
-app.use(bodyparser.json());
+//application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json 
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(()=> console.log('MongoDB connected...'))
-.catch(err=> console.log(err))
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
-app.get('/', (req,res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!~~ '))
 
 app.post('/register', (req,res) => {
 
     const user = new User(req.body)
 
-    user.save((err,userInfo) => {
-        if(err) return res.json({ success: false, err})
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
         return res.status(200).json({
-            success: true
+          success: true
         })
     })
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)) 
+app.post('/login', (req,res) => {
+
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "We cannot find an account with that e-mail address"
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "Your password is incorrect"})
+            
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id })
+            })
+        })
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
